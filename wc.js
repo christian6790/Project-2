b29430abb5511c6aa5c36b7c030fc382918cce54
christian6790 <31672571+christian6790@users.noreply.github.com>
2017-12-08 15:38:06 -0800

    Create wc.js

---
+++ wc.js
@@ -0,0 +1,15 @@
+function playDre(e){
+	document.getElementById("DDre").play();
+}
+
+function playcube(e){
+	document.getElementById("Ccube").play();
+}
+
+function playsnoop(e){
+	document.getElementById("G Thang").play();
+}
+
+function playEassy(e){
+	document.getElementById("Easy").play();
+}
