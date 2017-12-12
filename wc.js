3798412033bd41643e192b580348897ff78c88c7
christian6790 <31672571+christian6790@users.noreply.github.com>
2017-12-12 14:57:37 -0800

    Update wc.js

--- wc.js
+++ wc.js
@@ -1,19 +1,15 @@
 function playDre(e){
 	document.getElementById("DDre").play();
-	document.getElementById("DDre").pause();
 }
 
 function playcube(e){
 	document.getElementById("Ccube").play();
-	document.getElementById("Ccube").pause();
 }
 
 function playsnoop(e){
 	document.getElementById("G Thang").play();
-	document.getElementById("G Thang").pause();
 }
 
 function playEassy(e){
 	document.getElementById("Easy").play();
-	document.getElementById("Easy").pause();
 }
